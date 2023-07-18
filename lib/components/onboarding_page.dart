152c9316db825516662c13d45adcd0bb0814faa1
TheRealKwabena <gusasarkw@student.gu.se>
2023-07-18 22:58:21 +0200

    fix: changes extensions for the images and refactored the code for onboarding

---
+++ lib/components/onboarding_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:gradify_flutter/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'auth_button.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  final Color color;
+  final String text;
+  final String imageLink;
+
+  OnBoardingPage(
+      {required this.color, required this.text, required this.imageLink});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: Padding(
+        padding: const EdgeInsets.only(
+            top: 80.0, left: 20.0, right: 20.0, bottom: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              text,
+              style: GoogleFonts.montserrat(
+                  textStyle: kOnBoardingHeadingTextStyle),
+            ),
+            SizedBox(
+              height: 200.0,
+            ),
+            Container(
+              height: 223.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      imageLink,
+                    ),
+                    fit: BoxFit.fill),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                AuthenticationButton(
+                  buttonColor: Colors.black,
+                  text: 'Login',
+                  textColor: Colors.white,
+                ),
+                AuthenticationButton(
+                  buttonColor: Colors.white,
+                  text: 'Sign Up',
+                  textColor: Colors.black,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gradify_flutter/constants.dart';
-
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 import '../components/auth_button.dart';
-
+import '../components/onboarding_page.dart';
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
 
@@ -22,66 +22,3 @@ class OnBoarding extends StatelessWidget {
   }
 }
 
-class OnBoardingPage extends StatelessWidget {
-  final Color color;
-  final String text;
-  final String imageLink;
-
-  OnBoardingPage(
-      {required this.color, required this.text, required this.imageLink});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.only(
-            top: 80.0, left: 20.0, right: 20.0, bottom: 50.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              text,
-              style: GoogleFonts.montserrat(
-                  textStyle: kOnBoardingHeadingTextStyle),
-            ),
-            SizedBox(
-              height: 200.0,
-            ),
-            Container(
-              height: 223.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                      imageLink,
-                    ),
-                    fit: BoxFit.fill),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 100.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                AuthenticationButton(
-                  buttonColor: Colors.black,
-                  text: 'Login',
-                  textColor: Colors.white,
-                ),
-                AuthenticationButton(
-                  buttonColor: Colors.white,
-                  text: 'Sign Up',
-                  textColor: Colors.black,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
